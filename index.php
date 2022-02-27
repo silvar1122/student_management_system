cef51a404077f2e3b32215ba7762e7b20a626fea
silvar1122 <elizasenkoro@gmail.com>
2022-02-28 00:14:55 +0300

    Initial state of the website with html css and bootstrap

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management System</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <nav>
+        <label for="" class="logo">W-School</label>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Admission</a></li>
+            <li><a href="" class="btn btn-success">Login</a></li>
+        </ul>
+    </nav>
+
+    <div class="section_1">
+        <label class="image_text" for="">We Teach Students With Care</label>
+        <img src="school.jpg" alt="" class="main_image">
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="welcome_image" src="classroom.jpg" alt="">
+               
+            </div>
+            <div class="col-md-8">
+                <h1>Welcome to W School</h1>
+                <p>Heritage secondary school was established in January
+                     2011. It is located 56 kilometers from Dar es Salaam 
+                     along Morogoro road. 2 kilometers from kongowe center
+                      along Soga road. Heritage secondary is set in serene 
+                      environment at Vikuge, Soga ward. We offer conducive 
+                      leaning environment for student, away from the hustle 
+                      and bustle of the city life. Heritage secondary school 
+                      has a clear vision and commitment to combine academic 
+                      Excellence secondary Education following Tanzanian national 
+                      curriculum with well rounded development in spiritual, social and 
+                      intellectual process. The school is a full boarding Co- educational 
+                      Institution.</p>
+
+
+            </div>
+        </div>
+    </div>
+
+    <h1 class="teachers_title">Our Teachers</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+
+            <img class="teacher" src="teacher1.jpg" alt="">
+            <p>Heritage secondary school is dedicated to 
+                empowering students to become life-long learners,
+                 committed to honesty, equality, respect, integrity,
+                  trust, achievement, generosity and excellence. All 
+                  classrooms provide a student-centered, careful,
+                </p>
+            </div>
+
+            <div class="col-md-4">
+            <img class="teacher" src="teacher2.jpg" alt="">
+            <p>Heritage secondary school is dedicated to 
+                empowering students to become life-long learners,
+                 committed to honesty, equality, respect, integrity,
+                  trust, achievement, generosity and excellence. All 
+                  classrooms provide a student-centered, careful,
+                </p>
+            </div>
+
+            <div class="col-md-4">
+            <img  class="teacher" src="teacher3.jpg" alt="">
+            <p>Heritage secondary school is dedicated to 
+                empowering students to become life-long learners,
+                 committed to honesty, equality, respect, integrity,
+                  trust, achievement, generosity and excellence. All 
+                  classrooms provide a student-centered, careful,
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+    <h1 class="teachers_title">Our Courses</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+
+            <img class="teacher" src="science.jpg" alt="">
+            <h3 class="course_title">Web Development</h3>
+            </div>
+
+            <div class="col-md-4">
+            <img class="teacher" src="art.jpg" alt="">
+            <h3 class="course_title">Art</h3>
+            </div>
+
+            <div class="col-md-4">
+            <img  class="teacher" src="tech.jpg" alt="">
+            <h3 class="course_title">Tech Skills</h3>
+            </div>
+        </div>
+    </div>
+
+    
+        <h1  class="admission_title">Admission Form</h1>
+        <div class="admission_form">
+            <form action="">
+                <div class="imp_div">
+                    <label for="" class="label_text">Name</label>
+                    <input type="text" class="input_teg">
+                </div>
+
+                <div class="imp_div">
+                    <label for="" class="label_text">Email</label>
+                    <input type="text" class="input_teg">
+                </div>
+                <div class="imp_div">
+                    <label for="" class="label_text">Phone</label>
+                    <input type="text" class="input_teg">
+                </div>
+                <div class="imp_div">
+                    <label for="Message" class="label_text">Message</label>
+                    <textarea class="input_txt" name="" id="" cols="30" rows="10"></textarea>
+                </div>
+                <div>
+                    <input class="btn btn-primary" id="submit" type="submit" value="Apply">
+                </div>
+            </form>
+        </div>
+
+        <footer>
+            <h3>All @copyright reserved bt liztechnology</h3>
+        </footer>
+    
+</body>
+</html>
